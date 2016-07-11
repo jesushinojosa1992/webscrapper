d1ec4e0dfe8e2eaf8cdf33bcde0442a0630f83be
jesushinojosa1992 <jesushinojosa1992@gmail.com>
2016-07-10 18:20:03 -0700

    Each search is open on a new tab

---
+++ gsearch.py
@@ -0,0 +1,29 @@
+import webbrowser
+
+import numpy as np
+
+# Automated google searches opens new tabs with
+# key words from kewyword_searched.txt
+
+keyword_file = "keyword_searched.txt"
+
+
+def lines(keyword_file):
+
+	with open(keyword_file) as f:
+		keyword_lines = f.read().splitlines()
+	return keyword_lines
+
+keyword_lines = lines(keyword_file)
+
+print(keyword_lines[1])
+print(type(keyword_lines[1]))
+print("how many lines: ",len(keyword_lines))
+
+def search():
+	for i in range(len(keyword_lines)):
+		tabUrl = "http://google.com/?q="
+		umich=" University of Michigan Ann Arbor"
+		term=keyword_lines[i]
+		webbrowser.open(tabUrl+term+umich,new=i)
+search()
